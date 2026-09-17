aa53d29c536d26da34333dd1a79810cba6054d75
AHFAH <jiyou626@gmail.com>
2026-09-17 12:25:10 +0900

    ADD: 게시글 6개 작성

--- src/main/java/com/back/initData/DataInit.java
+++ src/main/java/com/back/initData/DataInit.java
@@ -4,11 +4,16 @@ import com.back.entity.Member;
 import com.back.entity.Post;
 import com.back.service.MemberService;
 import com.back.service.PostService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
 
+
+@Configuration
+@Slf4j
 public class DataInit {
     private final DataInit self;
     private final MemberService memberService;
@@ -32,7 +37,14 @@ public class DataInit {
             };
         }
 
-        public void makeBaseMembers(){
+        @Transactional
+        public void makeBaseMembers() {
+            if (memberService.count() > 0) return;
+
+            Member systemMember = memberService.join("system", "1234", "시스템");
+            Member holdingMember = memberService.join("holding", "1234", "홀딩");
+            Member adminMember = memberService.join("admin", "1234", "관리자");
+            Member user1Member = memberService.join("user1", "1234", "유저1");
             Member user2Member = memberService.join("user2", "1234", "유저2");
             Member user3Member = memberService.join("user3", "1234", "유저3");
         }
