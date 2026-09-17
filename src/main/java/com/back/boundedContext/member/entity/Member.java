c9b019c35608e85f8c3ba7857c013485c33498f9
AHFAH <jiyou626@gmail.com>
2026-09-17 14:17:49 +0900

    ADD: 이벤트리스너 도입

--- src/main/java/com/back/boundedContext/member/entity/Member.java
+++ src/main/java/com/back/boundedContext/member/entity/Member.java
@@ -1,17 +1,23 @@
 package com.back.boundedContext.member.entity;
 
-import com.back.boundedContext.global.jpa.entity.BaseIdAndTime;
+import com.back.global.jpa.entity.BaseIdAndTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@Getter
 public class Member extends BaseIdAndTime {
+
     @Column(unique = true)
     private String username;
+
     private String password;
+
     private String nickname;
+
     private int activityScore;
 
     public Member(String username, String password, String nickname) {
