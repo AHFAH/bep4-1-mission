45e39355c7f7a9c34ef96b7777bca462c64985c2
AHFAH <jiyou626@gmail.com>
2026-09-21 10:09:58 +0900

    UPDATE: CashEventListener의 비지니스 로직을 이벤트로 교체

--- src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
+++ src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
@@ -2,6 +2,7 @@ package com.back.boundedContext.cash.in;
 
 import com.back.boundedContext.cash.app.CashFacade;
 import com.back.boundedContext.cash.domain.CashMember;
+import com.back.shared.cash.event.CashMemberCreatedEvent;
 import com.back.shared.member.event.MemberJoinedEvent;
 import com.back.shared.member.event.MemberModifiedEvent;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +23,7 @@ public class CashEventListener {
     public void handle(MemberJoinedEvent event) {
         CashMember member = cashFacade.syncMember(event.getMember());
 
-        cashFacade.createWallet(member);
+        cashFacade.syncMember(event.getMember());
     }
 
     @TransactionalEventListener(phase = AFTER_COMMIT)
@@ -30,4 +31,12 @@ public class CashEventListener {
     public void handle(MemberModifiedEvent event) {
         cashFacade.syncMember(event.getMember());
     }
+
+
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(CashMemberCreatedEvent event) {
+        cashFacade.createWallet(event.getMember());
+    }
+
 }
